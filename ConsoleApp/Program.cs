1b0839e5afd4cd9d2eeb8f16e320a683bbcbefbc
Leina <68298361+leinahou@users.noreply.github.com>
2020-09-22 14:18:59 -0400

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using DUKPTCore;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bdk = "0123456789ABCDEFFEDCBA9876543210";
+            string ksn = "629949012C0000000003";
+            byte[] superSecretMessage = Encoding.UTF8.GetBytes("1234");
+            byte[] encryptedData = DUKPT.Encrypt(bdk, ksn, superSecretMessage, DUKPTVariant.PIN);
+            String hexres = BitConverter.ToString(encryptedData);
+            hexres = hexres.Replace("-", "");
+            Console.WriteLine("Encrypted Data: " + hexres);
+
+            byte[] decryptedData = DUKPT.Decrypt(bdk, ksn, encryptedData, DUKPTVariant.PIN);
+            string superSecretMessage1 = Encoding.UTF8.GetString(decryptedData);
+            Console.WriteLine("Decrypted Data: " + superSecretMessage1);
+        }
+    }
+}
